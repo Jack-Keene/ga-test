899f7eaebb0caba06fc5ca51a4ec648b010aee07
Jack Keene <jack.keene@snowplowanalytics.com>
2024-04-23 15:42:53 +0200

    add custom event

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-7NKJDVDSJJ");
+
 
 function App() {
   const [count, setCount] = useState(0)
+
+  function CustomEvent() {
+    setCount((count) => count + 1)
+    ReactGA.event({
+      category: "click",
+      action: "plus_one",
+      label: "button", // optional
+      value: count, // optional, must be a number
+      nonInteraction: true, // optional, true/false
+      transport: "xhr", // optional, beacon/xhr/image
+  });
+  }
 
   return (
     <>
@@ -18,7 +34,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => CustomEvent()}>
           count is {count}
         </button>
         <p>
